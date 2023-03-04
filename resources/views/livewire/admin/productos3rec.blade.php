fefcfe1c6f542f899062369177718dd743069ad7
ruben <perezrubentrabajos@gmail.com>
2023-03-04 19:14:56 +0100

    ARREGLO REFRACTOR DEL AÑO PASADO

--- resources/views/livewire/admin/productos3rec.blade.php
+++ resources/views/livewire/admin/productos3rec.blade.php
@@ -42,6 +42,17 @@
 
                     <th scope="col" class="relative px-6 py-3">
                         <span class="sr-only">Editar</span>
+                        Editar
+                    </th>
+
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        VENDIDOS
+                    </th>
+
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        PENDIENTES
                     </th>
                 </tr>
                 </thead>
