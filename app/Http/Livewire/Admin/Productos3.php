ca884ab39454568d24d5df424c55ac1e716eb156
ruben <perezrubentrabajos@gmail.com>
2023-03-06 13:39:12 +0100

    ***EJERCICIO 4 (están las demás cosas en Ejercicio 8 mitad y terminado)

--- app/Http/Livewire/Admin/Productos3.php
+++ app/Http/Livewire/Admin/Productos3.php
@@ -17,8 +17,10 @@ class Productos3 extends Component
     }
 
     public function render() {
-        $products = Product::where('name', 'LIKE', "%{$this->search}%")
-            ->paginate(10);
+        // $products = Product::where('name', 'LIKE', "%{$this->search}%")->paginate(10);
+        $products = Product::query()->applyFilters([
+            'search' => $this->search,
+        ])->paginate(10);
         return view('livewire.admin.productos3', compact('products'))
             ->layout('layouts.admin');
     }
